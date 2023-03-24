a5363f079cbe2c90f76f90504374e6c938b9b30c
Josias Hidalgo <jhidalgou@ucenfotec.ac.cr>
2023-03-23 22:52:28 -0600

    Creacion de la cards de validacion por OTP, falta hacerlo con JS

---
+++ WebApp - EasyList/wwwroot/js/ValidationAccount.js
@@ -0,0 +1,32 @@
+﻿function ValidationAccountView() {
+
+    this.ViewName = "ValidationAccountView";
+    //this.ApiService = "";
+    //var self = this;
+
+
+    this.InitView = function () {
+        console.log("Validation Account init");
+
+        //Asignacion del evento de clic
+        $("#btnValidaOTP").click(function () {
+            var view = new ValidationAccountView();
+            view.ValidaOTP();
+        });
+    }
+    this.ValidaOTP = function () {
+        // Cambia el contenido del <p>
+        document.getElementById('pValidaOTP').innerText = 'Digita el código que recibiste por SMS o correo electrónico.';
+
+    }
+    this.CleanForm = function () {
+
+    }
+}
+
+//Instaciamiento inicial de la clase
+//se ejecuta siempre al finalizar la carga de la vista
+$(document).ready(function () {
+    var view = new ValidationAccountView();
+    view.InitView();
+});
