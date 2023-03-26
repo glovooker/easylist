2fe8cff83a9892709bba0a8a39cc1e456f0a271e
Gabriel Lobo <globo.coding@gmail.com>
2023-03-25 22:44:59 -0600

    Users CRUD Finished

---
+++ DTOs/User.cs
@@ -0,0 +1,16 @@
+﻿namespace DTOs
+{
+    public class User : BaseEntity
+    {
+        public enum Status { ACTIVE, SUSPENDED, BANNED, DELETED }
+
+        public string name { get; set; }
+        public string firstLastName { get; set; }
+        public string secondLastName { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string userPicture { get; set; }
+        public DateTime registrationDate { get; set; }
+        public Status status { get; set; }
+    }
+}
---
+++ EasyListDataAccess/Mapper/ISqlStatements.cs
@@ -0,0 +1,15 @@
+﻿using DTOs;
+using EasyListDataAccess.DAOs;
+
+namespace EasyListDataAccess.Mapper
+{
+    public interface ISqlStatements
+    {
+        SqlOperation GetCreateStatement(BaseEntity entity);
+        SqlOperation GetRetrieveByIdStatement(int id);
+        SqlOperation GetRetrieveByEmailStatement(string email);
+        SqlOperation GetRetrieveAllStatement();
+        SqlOperation GetUpdateStatement(BaseEntity entity);
+        SqlOperation GetDeleteStatement(BaseEntity entity);
+    }
+}
