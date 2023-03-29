4ff6f4c428d7341bb97ec18e8fdedd9ade8031d2
Josias Hidalgo <jhidalgou@ucenfotec.ac.cr>
2023-03-28 19:39:27 -0600

    vista de validacion terminada

--- WebApp - EasyList/wwwroot/js/ValidationAccount.js
+++ WebApp - EasyList/wwwroot/js/ValidationAccount.js
@@ -8,19 +8,41 @@
     this.InitView = function () {
         console.log("Validation Account init");
 
+        $("#divCodigoOTP").hide();
+        $("#divVerificacionTerminada").hide();
+
         //Asignacion del evento de clic
         $("#btnValidaOTP").click(function () {
             var view = new ValidationAccountView();
             view.ValidaOTP();
         });
+        //Asignacion del evento de clic
+        $("#btnValidaCuenta").click(function () {
+            var view = new ValidationAccountView();
+            view.ValidaCuenta();
+        });
+    }
+    this.ValidaCuenta = function () {
+        if ($("#OTP-code").val() != "") {
+            $("#divCodigoOTP").hide();
+            $("#divVerificacionTerminada").show();
+
+        }
+        else {
+            console.log("No se ha ingresado el número de teléfono");
+        }
     }
     this.ValidaOTP = function () {
         // Cambia el contenido del <p>
-        document.getElementById('pValidaOTP').innerText = 'Digita el código que recibiste por SMS o correo electrónico.';
-
-    }
-    this.CleanForm = function () {
-
+        if ($("#country-code").val() != "" && $("#phone-number").val() != "") {
+           // document.getElementById('pValidaOTP').innerText = 'Digita el código que recibiste por SMS o correo electrónico.';
+            $("#divNumeroVerificacion").hide();
+            $("#divCodigoOTP").show();
+            
+        }
+        else {
+            console.log("No se ha ingresado el número de teléfono");
+        }
     }
 }
 
