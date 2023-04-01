bf3e487945ebd6402726547fae9bcc43f2a0b1a7
Luis Monge <lmongec@ucenfotec.ac.cr>
2023-03-31 20:01:53 -0600

    CRUD y la vista de la carga de los permisos en ManagePermisos

---
+++ DTOs/ManagePermission.cs
@@ -0,0 +1,9 @@
+﻿namespace DTOs
+{
+    public class ManagePermission : BaseEntity { 
+
+        public int user_id { get; set; }
+        public int permission_id { get; set; }
+
+    }
+}
---
+++ EasyListCORE/ManagePermissionManager.cs
@@ -0,0 +1,37 @@
+﻿using DTOs;
+using EasyListDataAccess.CRUD;
+
+namespace EasyListCORE
+{
+    public class ManagePermissionManager
+    {
+        //public void Create(Permission permission)
+        //{
+        //    var crudPermission = new PermissionCrudFactory();
+
+        //    crudPermission.Create(permission);
+        //}
+
+        //public void Delete(Permission permission)
+        //{
+        //    var crudPermission = new PermissionCrudFactory();
+        //    var existPermission = crudPermission.RetrieveById<Permission>(permission.Id);
+
+        //    if (existPermission == null)
+        //    {
+        //        throw new Exception("The permission does not exist!");
+        //    }
+
+        //    crudPermission.Delete(permission);
+        //}
+
+        public ManagePermission Check(ManagePermission managepermission)
+        {
+            var crudManagePermission = new ManagePermissionCrudFactory();
+            return crudManagePermission.CheckUserPermission<ManagePermission>(managepermission.user_id, managepermission.permission_id);
+
+        }
+
+
+    }
+}
