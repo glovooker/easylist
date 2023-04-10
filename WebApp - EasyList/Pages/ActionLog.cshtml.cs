0b89f88bd919b868f1feeb465c857ea1f1d30f45
Keylor Gómez Rodríguez <kgomezr@ucenfotec.ac.cr>
2023-04-09 20:06:18 -0600

    Create frontend for Action Log feature

---
+++ WebApp - EasyList/Pages/ActionLog.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp___EasyList.Pages
+{
+    public class ActionLogModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
