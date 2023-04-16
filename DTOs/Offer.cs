8ee22c9292c919192b4d8966c5fa54b56e665b79
Luis Monge <lmongec@ucenfotec.ac.cr>
2023-04-16 15:03:47 -0600

    CRUD y Vista general del modulo dew ofertas

---
+++ DTOs/Offer.cs
@@ -0,0 +1,15 @@
+﻿namespace DTOs
+{
+    public class Offer : BaseEntity
+    {
+        public int user_id { get; set; }
+        public int tender_id { get; set; }
+        public bool chosen { get; set; }
+        public float totalCost { get; set; }
+        public DateTime dueDate { get; set; }
+
+        public List<ProductOffer> ProductOffers { get; set; }
+
+    }
+
+}
---
+++ EasyListAPI/Controllers/OfferController.cs
@@ -0,0 +1,142 @@
+﻿using DTOs;
+using EasyListCORE;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyListAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OfferController : ControllerBase
+    {
+
+        [HttpPost]
+        [Route("createOffer")]
+        public async Task<IActionResult> CreateOffer(Offer offer)
+        {
+            try
+            {
+
+                var tm = new OfferManager();
+
+                tm.Create(offer);
+
+                return Ok();
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+        [HttpPut]
+        [Route("updateOffer")]
+        public async Task<IActionResult> UpdateOffer(Offer offer)
+        {
+            try
+            {
+
+                var tm = new OfferManager();
+
+                tm.Update(offer);
+
+                return Ok();
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete]
+        [Route("deleteOffer")]
+        public async Task<IActionResult> DeleteOffer(Offer offer)
+        {
+            try
+            {
+
+                var tm = new OfferManager();
+
+                tm.Delete(offer);
+
+                return Ok();
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("retrieveOfferById")]
+        public async Task<IActionResult> RetrieveOfferById(int id)
+        {
+            try
+            {
+
+                var tm = new OfferManager();
+
+                var offer = tm.RetrieveById(id);
+
+                return Ok(offer);
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+        [HttpGet]
+        [Route("retrieveAllOffers")]
+        public async Task<IActionResult> RetrieveAllOffers()
+        {
+            try
+            {
+                var tm = new OfferManager();
+
+                return Ok(tm.RetrieveAll());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("getOffersByDate")]
+        public async Task<IActionResult> RetrieveOffersByDate(string startDate, string endDate)
+        {
+            try
+            {
+                var um = new OfferManager();
+                var tenders = um.RetrieveByDate(startDate, endDate);
+                return Ok(tenders);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("checkUserOffer")]
+        public async Task<IActionResult> CheckUserOffer(int user, int tender)
+        {
+            try
+            {
+                var om = new OfferManager();
+                var offer = om.CheckUserOffer(user, tender);
+                return Ok(offer);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
