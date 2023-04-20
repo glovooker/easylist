6f082724a7e86848ac4234deb01e2427537bb296
Keylor Gómez Rodríguez <kgomezr@ucenfotec.ac.cr>
2023-04-20 15:04:40 -0600

    Se crea la lógica para listar los datos de ofertas para cada una de las licitaciones

--- WebApp - EasyList/wwwroot/js/OffersList.js
+++ WebApp - EasyList/wwwroot/js/OffersList.js
@@ -10,10 +10,53 @@
             view.Back();
         });
 
+        this.LoadTable();
+
     }
 
     this.LoadTable = function () {
         var ctrlActions = new ControlActions();
+        var tenderId = localStorage.getItem('tenderID');
+        var urlService = ctrlActions.GetUrlApiService(
+            this.ApiService + '/retrieveOffersByTenderId?id=' + tenderId
+        );
+
+        var arrayColumnsData = [];
+        arrayColumnsData[0] = { 'data': 'id' };
+        arrayColumnsData[1] = { 'data': 'user_id' };
+        arrayColumnsData[2] = { 'data': 'tender_id' };
+        arrayColumnsData[3] = {
+            'data': 'chosen',
+            'render': function (data) {
+                const statusMap = {
+                    false: 'No',
+                    true: 'Yes',
+                };
+                return statusMap[data] || data;
+            },
+        };
+        arrayColumnsData[4] = {
+            'data': 'dueDate',
+            'render': function (data) {
+                const isoDate = new Date(data);
+                const options = { day: '2-digit', month: '2-digit', year: 'numeric' };
+                return isoDate.toLocaleDateString('en-GB', options).replace(/\//g, '/');
+            },
+        };
+        arrayColumnsData[5] = {
+            'data': 'totalCost',
+            'render': function (data) {
+                return `$ ${data}`;
+            },
+        };
+
+        $('#tblOffer').dataTable({
+            'ajax': {
+                'url': urlService,
+                'dataSrc': '',
+            },
+            'columns': arrayColumnsData,
+        });
 
     };
 
