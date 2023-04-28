9ee8e8971144862389f22d5df5fcd37210e84144
Josias Hidalgo <jhidalgou@ucenfotec.ac.cr>
2023-04-27 19:21:20 -0600

    Logica de creacion de QR y envio al correo

--- EasyListCORE/NotificationManager.cs
+++ EasyListCORE/NotificationManager.cs
@@ -1,6 +1,9 @@
 ﻿using DTOs;
+using QRCoder;
 using SendGrid;
 using SendGrid.Helpers.Mail;
+using System.Drawing;
+using System.Drawing.Imaging;
 using Twilio;
 using Twilio.Rest.Api.V2010.Account;
 
@@ -8,6 +11,48 @@ namespace EasyListCORE
 {
     public class NotificationManager
     {
+
+        public async void NotifyQrByEmail(string email)
+        {
+            var apiKey = "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";
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("josias.andres@hotmail.com", "EasyList");
+            var subject = "QR de verificacion";
+            var to = new EmailAddress(email);
+            var plainTextContent = $"Hola, aquí tienes tu imagen QR";
+
+            // Generar el código QR
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode($"https://localhost:7110/{email}", QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+
+            // Convertir la imagen en un arreglo de bytes
+            byte[] imageBytes;
+            using (MemoryStream stream = new MemoryStream())
+            {
+                qrCodeImage.Save(stream, ImageFormat.Png);
+                imageBytes = stream.ToArray();
+            }
+
+            // Adjuntar la imagen al correo electrónico
+            var attachment = new Attachment
+            {
+                Filename = "qrcode.png",
+                Content = Convert.ToBase64String(imageBytes),
+                Type = "image/png",
+                Disposition = "attachment"
+            };
+
+            // Crear el correo electrónico
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, "<p><strong>Hola, aquí tienes tu imagen QR:</strong></p>");
+            msg.AddAttachment(attachment);
+
+            // Enviar el correo electrónico
+            var response = await client.SendEmailAsync(msg);
+        }
+        
         public void NotifyAccountValidation(Validation validation)
         {
             var user = new UserManager().RetrieveById(int.Parse(validation.userId));
@@ -54,6 +99,7 @@ namespace EasyListCORE
                 to: new Twilio.Types.PhoneNumber($"{phone}")
             );
         }
+
 
         private static string _sendGridApiKey = "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";
         private static EmailAddress _from = new EmailAddress("lmongec@ucenfotec.ac.cr", "EasyList");
